45cb7042738b77e3622976dffaad493f6466e15d
Kishore1071 <kishore240398@gmail.com>
2023-10-12 14:11:06 +0530

    Updated

--- Basics/4__ListMethods.py
+++ Basics/4__ListMethods.py
@@ -4,59 +4,83 @@ number_list = [43,12,67,9,34,75,34,5]
 
 # Accessing  [This is the only method available for tuple datatype]
 
-animals[3]
+print(animals[3])
 
 
 # Updating
 
 animals[1] = "Wild Dog"
 
+print(animals)
+
 
 # Add as last element
 
 animals.append("Elephant")
+
+print(animals)
+
 
 
 # Remove last element
 
 animals.pop()
 
+print(animals)
+
+animals.pop(2)
+
+print(animals)
+
 
 # Add at selected index
 
 animals.insert(1, "Polar Bear")
 
+print(animals)
 
-# Remove selected index
 
-animals.pop(4)
+# # Remove selected index
+
+# animals.pop(4)
 
 
 # Asending order
 
 number_list.sort()
 
+print(number_list)
+
 
 # Desending order
 
 number_list.sort(reverse=True)
+
+print(number_list)
 
 
 # Empty a list
 
 number_list.clear()
 
+print(number_list)
+
 
 # Adding list
 
+data_set1 = [1,2,3]
+data_set2 = [1,2,3]
+
 new_dataset = data_set1 + data_set2
+
+print(new_dataset)
 
 
 # Find the index of a value
 
-animals.index("Dear")
+print(animals.index("Dear"))
 
 
 # Length
 
-len(animals)
+print(len(animals))
--- Basics/5__DictionaryMethods.py
+++ Basics/5__DictionaryMethods.py
@@ -4,42 +4,53 @@ laptop = {
     "launched_year": 2013
 }
 
+print(laptop)
+
 
 # Accessing
 
-laptop["brand"]
+print(laptop["brand"])
 
 
 # Updating
 
 laptop['model'] = "Dell Inspiron 3542"
 
+print(laptop)
+
 
 # View Keys
 
-laptop.keys()
+print(laptop.keys())
 
 
 # View values
 
-laptop.values()
+print(laptop.values())
 
 
 # Add new key value pair
 
 laptop['default_ram'] = "4GB"
 
+print(laptop)
+
 
 # Deleting last key value pair
 
 laptop.popitem()
+
+print(laptop)
 
 
 # Deleting selected key value pair
 
 laptop.pop("brand")
 
+print(laptop)
 
 # Remove all elements from dictionary
 
 laptop.clear()
+
+print(laptop)
